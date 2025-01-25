74137fdef2e461081c6a015872e895510495e890
Martin Schober <m.schober@fz-juelich.de>
2025-01-25 15:16:25 +0100

    implemented sync main

--- src/nashome/_cmd/sync_savegames.py
+++ src/nashome/_cmd/sync_savegames.py
@@ -2,16 +2,16 @@
 import argparse
 from pathlib import Path
 
-from nashome.photos.photo_handler import fix_photos
+from nashome.savegames.savegame_handler import sync_savegames
 
 def main():
     # argument parsing
-    parser = argparse.ArgumentParser(description="Rename photos and add datetime meta information.", formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('inpath', type=Path, help="Path to the input directory.")
-    parser.add_argument('-d', '--disable-synology', action='store_true', help="Disable Synology specific features.")
+    parser = argparse.ArgumentParser(description="Synchronize savegames from source directory to destination directory", formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('source', type=Path, help="Path to the source directory (JKSV-root)")
+    parser.add_argument('destination', type=Path, help="Path to the destination directory (JKSV-root)")
     args = parser.parse_args()
 
-    fix_photos(path=args.inpath, disable_synology=args.disable_synology)
+    sync_savegames(source_root_dir=args.source, dest_root_dir=args.destination)
 
 if __name__ == "__main__":
     main()
