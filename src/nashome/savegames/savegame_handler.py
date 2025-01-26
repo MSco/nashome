d4db0e8301cd0bed320c7d3d62854a6765b8922c
Martin Schober <m.schober@fz-juelich.de>
2025-01-26 11:43:45 +0100

    added def sync to sync subdirs

--- src/nashome/savegames/savegame_handler.py
+++ src/nashome/savegames/savegame_handler.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import shutil
 
 def sync_savegames(source_root_dir: str|Path, dest_root_dir: str|Path) -> None:
     """
@@ -25,13 +25,20 @@ def sync_savegames(source_root_dir: str|Path, dest_root_dir: str|Path) -> None:
     # Iterate over the source game directories
     for source_game_dir in source_root_dir.iterdir():
         dest_game_dir = dest_root_dir / source_game_dir.name
-        if not dest_game_dir.exists():
-            print(f"'{dest_game_dir}' not found. Skipping.")
+        if not dest_game_dir.is_dir():
+            print(f"'{dest_game_dir}' is not a directory. Skipping.")
             continue
-        
+
         # Get the latest date directory
-        source_date_dir = sorted(source_game_dir.iterdir())[-1]
-        dest_date_dir = sorted(dest_game_dir.iterdir())[-1]
+        source_date_dir = [f for f in sorted(source_game_dir.iterdir()) if f.name.startswith("Martin")]
+        dest_date_dir = [f for f in sorted(dest_game_dir.iterdir()) if f.name.startswith("Martin")]
+
+        if not len(source_date_dir) or not len(dest_date_dir):
+            print(f"Empty directory found. Skipping.")
+            continue
+
+        source_date_dir = source_date_dir[-1]
+        dest_date_dir = dest_date_dir[-1]
 
         # Check if the directories are valid
         if not source_date_dir.is_dir():
@@ -47,5 +54,18 @@ def sync_savegames(source_root_dir: str|Path, dest_root_dir: str|Path) -> None:
         for source_file in source_date_dir.iterdir():
             dest_file = dest_date_dir / source_file.name
             
-            print(f"Copying '{source_file}' to '{dest_file}'")
-            dest_file.write_bytes(source_file.read_bytes())
+            # start recursion
+            sync(source_file, dest_file)
+
+def sync(source:Path, dest:Path):
+    if source.is_file():
+        print(f"Copying '{source}' to '{dest}'")
+        shutil.copy(source, dest)
+    elif source.is_dir() and dest.is_dir():
+        for source_file in source.iterdir():
+            dest_file = dest / source_file.name
+            sync(source_file, dest_file)
+    else:
+        raise ValueError(f"Error while syncing from {source} to {dest}")
+    
+
