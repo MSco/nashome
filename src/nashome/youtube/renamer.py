fe37e1c1d3fc2ec21817b1d7d443f376d9f35b17
Martin Schober <m.schober@fz-juelich.de>
2025-02-05 10:58:13 +0100

    implemented build_filename_for_epgcontent, build_filename, extract_episode_name_from_youtube, extract_episode_name_from_epgcontent

--- src/nashome/youtube/renamer.py
+++ src/nashome/youtube/renamer.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from pytubefix import YouTube
 import re
 import requests
@@ -6,11 +7,18 @@ from unidecode import unidecode
 from nashome.config.config import tmdb_api_token
 
 def build_filename_for_youtube(yt:YouTube, audio_only:bool):
-    title = yt.title
     suffix = 'm4a' if audio_only else 'mp4'
-    return build_filename(title=title, suffix=suffix, language_code='en-US')
+    episode_name = extract_episode_name_from_youtube(yt.title)
+    return build_filename(original_title=yt.title, episode_name=episode_name, suffix=suffix, language_code='en-US')
 
-def build_filename(title:str, suffix:str, language_code:str):
+def build_filename_for_epgcontent(filename:str, content:str):
+    episode_name = extract_episode_name_from_epgcontent(content)
+    file_stem = Path(filename).stem
+    if not episode_name:
+        episode_name = file_stem
+    return build_filename(original_title=file_stem, episode_name=episode_name, suffix="ts", language_code='de-DE')
+
+def build_filename(original_title:str, episode_name:str, suffix:str, language_code:str):
     # dict = { series_name: [series_id, num_seasons] }
     dict_series = {
         'Pokemon Horizonte': [220150, 1],
@@ -19,19 +27,28 @@ def build_filename(title:str, suffix:str, language_code:str):
     
     output_filename = None
     for series_name in dict_series.keys():
-        if filter_string(series_name) in filter_string(title):
-            episode, season = find_episode_and_season(title=title, series_id=dict_series[series_name][0], num_seasons=dict_series[series_name][1], language_code=language_code)
+        if filter_string(series_name) in filter_string(original_title):
+            episode, season = find_episode_and_season(title=episode_name, series_id=dict_series[series_name][0], num_seasons=dict_series[series_name][1], language_code=language_code)
             if not episode or not season:
                 continue
             output_filename = f'{series_name} - s{season:02d}e{episode:03d}.{suffix}'
             break
     else:
-        output_filename = f'{title}.{suffix}'
+        output_filename = f'{episode_name}.{suffix}'
 
     return output_filename
 
-def extract_episode_name(title:str):
+def extract_episode_name_from_youtube(title:str) -> str:
     episode_name = filter_string(title)
+    return episode_name.split("|")[0].strip()
+
+def extract_episode_name_from_epgcontent(content:str) -> str:
+    filtered_content = filter_string(content)
+    match_episode = re.match(r".*<x>schedule</x>(.*?)\x00", filtered_content)
+    if match_episode is not None:
+        return match_episode.group(1)
+    
+    return None
 
 def find_episode_and_season(title:str, series_id:int, num_seasons:int, language_code:str):
     # https://developer.themoviedb.org/reference/search-tv
