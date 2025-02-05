8a9ade47623a23c1cc7a97a51c6c92dbbe55d0a1
Martin Schober <m.schober@fz-juelich.de>
2025-02-05 10:39:20 +0100

    added num_seasonst to dict, use new parameter set

--- src/nashome/youtube/renamer.py
+++ src/nashome/youtube/renamer.py
@@ -8,26 +8,30 @@ from nashome.config.config import tmdb_api_token
 def build_filename_for_youtube(yt:YouTube, audio_only:bool):
     title = yt.title
     suffix = 'm4a' if audio_only else 'mp4'
-    return build_filename(title=title, suffix=suffix)
+    return build_filename(title=title, suffix=suffix, language_code='en-US')
 
-def build_filename(title:str, suffix:str):
+def build_filename(title:str, suffix:str, language_code:str):
+    # dict = { series_name: [series_id, num_seasons] }
     dict_series = {
-        'Pokemon Horizonte': 220150,
-        'Pokemon': 60572,
+        'Pokemon Horizonte': [220150, 1],
+        'Pokemon': [60572, 25]
     }
     
     output_filename = None
-    for title_name in dict_series.keys():
-        if filter_string(title_name) in filter_string(title):
-            episode, season = find_episode_and_season(title=title, series_id=dict_series[title_name])
+    for series_name in dict_series.keys():
+        if filter_string(series_name) in filter_string(title):
+            episode, season = find_episode_and_season(title=title, series_id=dict_series[series_name][0], num_seasons=dict_series[series_name][1], language_code=language_code)
             if not episode or not season:
                 continue
-            output_filename = f'{title_name} - s{season:02d}e{episode:03d}.{suffix}'
+            output_filename = f'{series_name} - s{season:02d}e{episode:03d}.{suffix}'
             break
     else:
         output_filename = f'{title}.{suffix}'
 
     return output_filename
+
+def extract_episode_name(title:str):
+    episode_name = filter_string(title)
 
 def find_episode_and_season(title:str, series_id:int, num_seasons:int, language_code:str):
     # https://developer.themoviedb.org/reference/search-tv
