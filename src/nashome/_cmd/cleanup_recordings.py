2a08f67e494ccc4dc510a90712b29de0ea8aeca6
Martin Schober <m.schober@fz-juelich.de>
2025-02-05 12:31:37 +0100

    cleanup-recordings: moved code to module, use argparse

--- src/nashome/_cmd/cleanup_recordings.py
+++ src/nashome/_cmd/cleanup_recordings.py
@@ -5,100 +5,21 @@ Created on 13.07.2022
 @author: mschober
 '''
 
-import os
-import click
-import sys
-import re
+import argparse
+from pathlib import Path
 
-@click.command()
-@click.argument('files', nargs=-1)
-@click.option('-s', '--series', is_flag=True, help="Set this flag to rename series")
-@click.option('-d', '--dash', is_flag=True, help="Set this flag if the series/movie name contains a dash")
+from nashome.utils.renamer import cleanup_recordings
 
-def main(files:list[str], series:bool, dash:bool):
-    #     path = '/nashome/Serien/Die tollen Fußballstars/'
-    extensions = ('.eit', '.ts', '.meta', '.jpg', '.txt')
-    remove_extensions = ('.ap', '.cuts', '.sc', 'idx2')
+def main():
+    # argument parsing
+    parser = argparse.ArgumentParser(description="Rename recordings.", formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('files', type=Path, nargs='+', help="YouTube movie/playlist url(s).")
+    parser.add_argument('-s', "--series", action='store_true', help="Set this flag to rename series.")
+    parser.add_argument('-d', "--dash", type=str, help="Set this flag if the series/movie name contains a dash.")
     
-    regex_eit = re.compile(b"^.*([0-9]+)\. Staffel, Folge ([0-9]+).*")
-    regex_filename = re.compile(r"^.* - (.*) - (.*)\.eit" if dash else r"^.* - (.*)\.eit")
-    
-    remove_list = []
-    rename_dict = {}
-    touch_oldname_list = []
-    
-    for path in files:
-        root:str = os.path.dirname(path)
-        filename:str = os.path.basename(path)
-        
-        if filename.endswith(remove_extensions):
-            remove_list.append(path)
-        elif filename.endswith('eit'):
-            match = regex_filename.match(filename.replace('_', ' '))
-            name = match.group(1) + " - " + match.group(2) if dash else match.group(1)
-            
-            if series:
-                with open(path, mode='rb') as file:
-                    eit_content = file.read()
-            
-                match = regex_eit.match(eit_content)
-                season = int(match.group(1).decode())
-                episode = int(match.group(2).decode())
-                    
-                newname = "{} - s{:02d}e{:03d}".format(name, season, episode)
-            else:
-                newname = name
-                
-            basename = os.path.splitext(filename)[0]
-            
-            for oldname in [f for f in files if f.startswith(basename) and f.endswith(extensions)]:
-                ext = os.path.splitext(oldname)[1]
-                oldpath = os.path.join(root, oldname)
-                newpath = os.path.join(root, newname + ext)
-                rename_dict[oldpath] = newpath
-                if not series and ext == '.ts':
-                    touch_oldname_list.append(oldpath)
-    
-    if len(remove_list)==0 and len(rename_dict)==0:
-        print("Nothing to do")
-        sys.exit(0)
-         
-    for remove_path in remove_list:
-        print("delete: {}".format(remove_path))
-        
-    for rename_path in rename_dict:    
-        print("rename: {} -> {}".format(rename_path, rename_dict[rename_path]))
-        
-    for touch_path in touch_oldname_list: 
-        oldname = os.path.basename(touch_path)
-        txt_path = os.path.splitext(touch_path)[0] + '.oldname.txt'   
-        print("create '{}' with content '{}'".format(txt_path, oldname))
-        
-    value_list = list(rename_dict.values())
-    if (len(value_list) > len(set(value_list))):
-        print("Aborting... Some files would be overwritten. Please see renaming list and check your input.") 
-        sys.exit(1)
+    args = parser.parse_args()
 
-    print("Commands ok? [Y,n]")
-    response = input()
-    if len(response) == 0 or response.lower() == 'y':
-        for remove_path in remove_list:
-            print("removing {}".format(remove_path))
-            os.remove(remove_path)
-        for rename_path in rename_dict:    
-            print("renaming {} -> {}".format(rename_path, rename_dict[rename_path]))
-            os.rename(rename_path, rename_dict[rename_path])
-        for touch_path in touch_oldname_list:
-            oldname = os.path.basename(touch_path)
-            txt_path = os.path.splitext(touch_path)[0] + '.oldname.txt'
-            print("creating {}".format(txt_path))
-            f = open(txt_path, "w")
-            f.write(oldname)
-    else:
-        print("Nothing done!")
-            
-    
-if __name__ == '__main__':
+    cleanup_recordings(paths=args.files, series=args.series, dash=args.dash)
+
+if __name__ == "__main__":
     main()
-    sys.exit(0)
-
--- src/nashome/utils/renamer.py
+++ src/nashome/utils/renamer.py
@@ -6,37 +6,49 @@ from unidecode import unidecode
 
 from nashome.config.config import tmdb_api_token
 
-def build_filename_for_youtube(yt:YouTube, audio_only:bool):
+def build_filename_from_youtube(yt:YouTube, audio_only:bool):
     suffix = 'm4a' if audio_only else 'mp4'
     episode_name = extract_episode_name_from_youtube(yt.title)
-    return build_filename(original_title=yt.title, episode_name=episode_name, suffix=suffix, language_code='en-US')
+    filestem = build_filestem(original_title=yt.title, episode_name=episode_name, language_code='en-US')
+    return f"{filestem}.{suffix}"
 
-def build_filename_for_epgcontent(filename:str, content:str):
-    episode_name = extract_episode_name_from_epgcontent(content)
-    file_stem = Path(filename).stem
-    if not episode_name:
-        episode_name = file_stem
-    return build_filename(original_title=file_stem, episode_name=episode_name, suffix="ts", language_code='de-DE')
+def build_filestem_from_epgfile(series_name:str, epg_path:str|Path):
+    regex_epg = re.compile(b"^.*([0-9]+)\. Staffel, Folge ([0-9]+).*")
+    
+    epg_file = open(epg_path, mode='rb')
+    epg_content = epg_file.read()
+    
+    match_epg = regex_epg.match(epg_content)
+    if match_epg is not None: 
+        season = int(match_epg.group(1).decode())
+        episode = int(match_epg.group(2).decode())
+        
+        return f"{series_name} - s{season:02d}e{episode:03d}"
+    else:
+        episode_name = extract_episode_name_from_epgcontent(epg_content)
+        if not episode_name:
+            episode_name = series_name
+        return build_filestem(original_title=series_name, episode_name=episode_name, language_code='de-DE')
 
-def build_filename(original_title:str, episode_name:str, suffix:str, language_code:str):
+def build_filestem(original_title:str, episode_name:str, language_code:str):
     # dict = { series_name: [series_id, num_seasons] }
     dict_series = {
         'Pokemon Horizonte': [220150, 1],
         'Pokemon': [60572, 25]
     }
     
-    output_filename = None
+    output_filestem = None
     for series_name in dict_series.keys():
         if filter_string(series_name) in filter_string(original_title):
             episode, season = find_episode_and_season(title=episode_name, series_id=dict_series[series_name][0], num_seasons=dict_series[series_name][1], language_code=language_code)
             if not episode or not season:
                 continue
-            output_filename = f'{series_name} - s{season:02d}e{episode:03d}.{suffix}'
+            output_filestem = f'{series_name} - s{season:02d}e{episode:03d}'
             break
     else:
-        output_filename = f'{episode_name}.{suffix}'
+        output_filestem = f'{episode_name}'
 
-    return output_filename
+    return output_filestem
 
 def extract_episode_name_from_youtube(title:str) -> str:
     episode_name = filter_string(title)
@@ -92,3 +104,74 @@ def filter_string(string:str|bytes) -> str:
         filtered_string = re.sub(key, value, filtered_string)
 
     return filtered_string.strip()
+
+def cleanup_recordings(paths:list[Path], series:bool, dash:bool):
+    extensions = ('.eit', '.ts', '.meta', '.jpg', '.txt')
+    remove_extensions = ('.ap', '.cuts', '.sc', 'idx2')
+    
+    regex_filename = re.compile(r"^.* - (.*) - (.*)\.eit" if dash else r"^.* - (.*)\.eit")
+    
+    remove_list:list[Path] = []
+    rename_dict:dict[Path, Path] = {}
+    touch_oldname_list:list[Path] = []
+    
+    for path in paths:
+        root = path.parent
+        filename = path.name
+        
+        if filename.endswith(remove_extensions):
+            remove_list.append(path)
+        elif filename.endswith('eit'):
+            match_filename = regex_filename.match(filename.replace('_', ' '))
+            name = match_filename.group(1) + " - " + match_filename.group(2) if dash else match_filename.group(1)
+            
+            if series:
+                newstem = build_filestem_from_epgfile(name, path)
+            else:
+                newstem = name
+                
+            basename = path.stem
+            
+            for oldpath in [p for p in paths if p.name.startswith(basename) and p.name.endswith(extensions)]:
+                suffix = "".join(oldpath.suffixes)
+                newpath = root/(newstem + suffix)
+                rename_dict[oldpath] = newpath
+                if not series and suffix == '.ts':
+                    touch_oldname_list.append(oldpath)
+    
+    if len(remove_list)==0 and len(rename_dict)==0:
+        print("Nothing to do")
+        return True
+         
+    for remove_path in remove_list:
+        print(f"delete: {remove_path}")
+        
+    for rename_path in rename_dict:    
+        print(f"rename: {rename_path} -> {rename_dict[rename_path]}")
+        
+    for touch_path in touch_oldname_list: 
+        txt_path = touch_path.with_suffix('.oldname.txt')
+        print(f"create '{txt_path}' with content '{touch_path.name}'")
+        
+    value_list = list(rename_dict.values())
+    if (len(value_list) > len(set(value_list))):
+        print("Aborting... Some files would be overwritten. Please see renaming list and check your input.") 
+        return False
+
+    print("Commands ok? [Y,n]")
+    response = input()
+    if len(response) == 0 or response.lower() == 'y':
+        for remove_path in remove_list:
+            print("removing {}".format(remove_path))
+            remove_path.unlink()
+        for rename_path in rename_dict:    
+            print(f"renaming {rename_path} -> {rename_dict[rename_path]}")
+            rename_path.rename(rename_dict[rename_path])
+        for touch_path in touch_oldname_list:
+            oldname = touch_path.name
+            txt_path = touch_path.with_suffix('.oldname.txt')
+            print(f"creating {txt_path}")
+            f = open(txt_path, "w")
+            f.write(oldname)
+    else:
+        print("Nothing done!")
