f16a58538004757fbecae00256535fe78ba337bd
Martin Schober <m.schober@fz-juelich.de>
2025-02-06 14:23:08 +0100

    added class Series

---
+++ src/nashome/utils/series.py
@@ -0,0 +1,19 @@
+import re
+
+class Series():
+    def __init__(self, name:str, series_id:int, regex:str=r"(.*)"):
+        self.name = name
+        self.series_id = series_id
+        self.regex = regex
+
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return self.name
+    
+    def __eq__(self, other):
+        return self.name == str(other)
+    
+    def build_episode_name(self, title:str) -> str:
+        return re.match(self.regex, title).group(1).strip()
