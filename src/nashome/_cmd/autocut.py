22892543469a9d6adae5f98eaab3a3f7bd3df027
Martin Schober <m.schober@fz-juelich.de>
2025-02-06 09:45:41 +0100

    add autocut application

---
+++ src/nashome/_cmd/autocut.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import argparse
+from pathlib import Path
+
+from nashome.utils.movie import cut_video
+
+def main():
+    # argument parsing
+    parser = argparse.ArgumentParser(description="Cut movie by given start and end templates", formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('movie', type=Path, help="Path to the movie file.")
+    parser.add_argument('start-template', type=Path, help="Path to the start template image file.")
+    parser.add_argument('end-template', type=Path, help="Path to the end template image file.")
+    parser.add_argument('outdir', type=Path, help="Path to the output directory.")
+    parser.add_argument('-o', "--offset", type=float, help="Set the start offset of the movie in minutes.")
+    parser.add_argument('-l', "--length", type=float, help="Set the length of the movie in minutes.")
+    
+    args = parser.parse_args()
+
+    cut_video(video_path=args.movie, 
+              start_template_path=args.start_template, 
+              end_template_path=args.end_template, 
+              output_path=args.outdir, 
+              offset_minutes=args.offset, 
+              movie_length_minutes=args.length)
+
+if __name__ == "__main__":
+    main()
