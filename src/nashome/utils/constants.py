7cf175200eea73e0c9d8f416d03792d3d8c01c8a
Martin Schober <m.schober@fz-juelich.de>
2025-02-06 14:38:13 +0100

    added regex to kickers, added more series

--- src/nashome/utils/constants.py
+++ src/nashome/utils/constants.py
@@ -30,11 +30,18 @@ LANGUAGE_LIST:list[Language] = [
 SERIES_LIST:list[Series] = [
     Series("Pokemon Horizonte", 220150),
     Series("Pokemon", 60572, r"(.*?)\|.*"),
-    Series("Die Kickers", 64049),
+    Series("Die Kickers", 64049, r"Die Kickers - (.*) Folge \d+"),
     Series("Paw Patrol", 57532, r".*\|(.*)\|.*"),
     Series("PJ Masks", 65417, r"(?:Ganze Folge:*)?(.*?)[^\u0000-\uFFFF].*"),
     Series("My Hero Academia", 65930),
-    Series("SpongeBob Schwammkopf", 387)
+    Series("SpongeBob Schwammkopf", 387),
+    Series("Desperate Housewives", 693),
+    Series("Sex and the City", 105),
+    Series("And Just Like That", 116450),
+    Series("The Big Bang Theory", 1418),
+    Series("House of the Dragon", 94997),
+    Series("Game of Thrones", 1399),
+    Series("The Lazarus Project", 194567)
 ]
 
 STORED_VIDEOS_FILENAME = "stored_videos.json"
