ea75940d6f1bab9905f66fb05b8e6e1fa6a47f4d
Martin Schober <m.schober@fz-juelich.de>
2025-02-06 10:02:00 +0100

    fixed template names

--- src/nashome/_cmd/autocut.py
+++ src/nashome/_cmd/autocut.py
@@ -8,17 +8,17 @@ def main():
     # argument parsing
     parser = argparse.ArgumentParser(description="Cut movie by given start and end templates", formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('movie', type=Path, help="Path to the movie file.")
-    parser.add_argument('start-template', type=Path, help="Path to the start template image file.")
-    parser.add_argument('end-template', type=Path, help="Path to the end template image file.")
+    parser.add_argument('start', metavar="start-template", type=Path, help="Path to the start template image file.")
+    parser.add_argument('end', metavar="end-template", type=Path, help="Path to the end template image file.")
     parser.add_argument('outdir', type=Path, help="Path to the output directory.")
-    parser.add_argument('-o', "--offset", type=float, help="Set the start offset of the movie in minutes.")
+    parser.add_argument('-o', "--offset", type=float, default=0, help="Set the start offset of the movie in minutes.")
     parser.add_argument('-l', "--length", type=float, help="Set the length of the movie in minutes.")
     
     args = parser.parse_args()
 
     cut_video(video_path=args.movie, 
-              start_template_path=args.start_template, 
-              end_template_path=args.end_template, 
+              start_template_path=args.start, 
+              end_template_path=args.end, 
               outdir=args.outdir, 
               offset_minutes=args.offset, 
               movie_length_minutes=args.length)
