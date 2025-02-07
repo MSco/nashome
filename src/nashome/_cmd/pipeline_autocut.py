1e87f2cf64e10ef28c4afa7316fcacf260d6c2d9
Martin Schober <m.schober@fz-juelich.de>
2025-02-07 14:49:16 +0100

    implemented main pipeline-autocut and added to toml

---
+++ src/nashome/_cmd/pipeline_autocut.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import argparse
+from pathlib import Path
+
+from nashome.utils.pipeline import cleanup_and_autocut
+
+def main():
+    # argument parsing
+    parser = argparse.ArgumentParser(description="Copy recordings, rename them and autocut", formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('recordings', metavar="recordings-root-directory", type=Path, help="Path to the recordings autocut input directory.")
+    parser.add_argument('template', metavar="template-directory", type=Path, help="Path to the template image file directory.")
+    parser.add_argument('outdir', metavar="outdir-root-directory", type=Path, help="Path to the series output root directory.")
+    
+    args = parser.parse_args()
+
+    cleanup_and_autocut(recordings_root_path=args.recordings, 
+                        template_directory=args.template, 
+                        outdir_root_path=args.outdir)
+
+if __name__ == "__main__":
+    main()
